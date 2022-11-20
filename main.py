dcc141715b7d79c9516eb70f3a46b16958e6c8f5
VladislavInkov1 <vladislav@inkov.ru>
2022-11-20 20:33:08 +0300

    cell verification has been optimized, the ability to enter the number of cell friends and the frequency of matrix saving has been added

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import numpy as np
 import random
 import matplotlib.pyplot as plt
+import os
 
 n = int(input('Введите размерность матрицы: '))
 iter_k = int(input('Введите число итераций: '))
+number_of_friends = int(input('Сколько друзей нужно клетке для счастья? '))
+iteration_frequency = int(input('Через сколько итераций сохранять матрицу? '))
+
+
+
+def delete_previous_experiment():
+    for f in os.listdir('img_iteration/'):
+        os.remove(os.path.join('img_iteration/', f))
 
 
 # generating an n*n matrix with a random distribution of cells
@@ -13,22 +22,30 @@ def matrix_creation(n):
     return matrix
 
 
+# checks if the cell is unhappy
+def is_unhappy_cell(i, j, matrix):
+    offsets = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+    friends_counter = 0
+    for di, dj in offsets:
+        ni, nj = i + di, j + dj
+        if ni in range(len(matrix)) and nj in range(len(matrix)) and matrix[ni][nj] == matrix[i][j]:
+            friends_counter += 1
+    if friends_counter >= number_of_friends:
+        return False
+    else:
+        return True
+
+
 # selection of unhappy and empty cells
 def cell_separation(matrix):
     unhappy_cells = []
     empty_cells = []
-    offsets = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
     for i, row in enumerate(matrix):
         for j, column in enumerate(row):
-            friends_counter = 0
             if matrix[i][j] == (255, 255, 255):
                 empty_cells.append((i, j))
             else:
-                for di, dj in offsets:
-                    ni, nj = i + di, j + dj
-                    if ni in range(len(matrix)) and nj in range(len(matrix)) and matrix[ni][nj] == matrix[i][j]:
-                        friends_counter += 1
-                if friends_counter < 3:
+                if is_unhappy_cell(i, j, matrix):
                     unhappy_cells.append((i, j))
     return unhappy_cells, empty_cells
 
@@ -50,30 +67,48 @@ def matrix_conversion(matrix):
 def choosing_migrant(matrix, unhappy_cells, empty_cells):
     old_address = random.randint(0, len(unhappy_cells) - 1)
     new_adress = random.randint(0, len(empty_cells) - 1)
-    try:
-        new_a_i, new_a_j = unhappy_cells[new_adress][1], unhappy_cells[new_adress][1]
-        old_a_i, old_a_j = empty_cells[new_adress][1], empty_cells[new_adress][1]
-        matrix[new_a_i][new_a_j], matrix[old_a_i][old_a_j] = matrix[old_a_i][old_a_j], matrix[new_a_i][new_a_j]
-    except:
-        print('Все клетки счастливы!')
-    return matrix
+    new_a_i, new_a_j = empty_cells[new_adress][0], empty_cells[new_adress][1]
+    old_a_i, old_a_j = unhappy_cells[old_address][0], unhappy_cells[old_address][1]
+    matrix[new_a_i][new_a_j], matrix[old_a_i][old_a_j] = matrix[old_a_i][old_a_j], matrix[new_a_i][new_a_j]
+    empty_cells.pop(new_adress)
+    empty_cells.append(unhappy_cells[old_address])
+    unhappy_cells.pop(old_address)
+    if is_unhappy_cell(new_a_i, new_a_j, matrix):
+        unhappy_cells.append((new_a_i, new_a_j))
+    offsets = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+    for di, dj in offsets:
+        ni, nj = new_a_i + di, new_a_j + dj
+        if ni in range(len(matrix)) and nj in range(len(matrix)):
+            if is_unhappy_cell(ni, nj, matrix) and matrix[ni][nj] != (255, 255, 255) and not((ni, nj) in unhappy_cells):
+                unhappy_cells.append((ni, nj))
+            elif not(is_unhappy_cell(ni, nj, matrix)) and (ni, nj) in  unhappy_cells:
+                unhappy_cells.remove((ni, nj))
+        oi, oj = old_a_i + di, old_a_j + dj
+        if oi in range(len(matrix)) and oj in range(len(matrix)):
+            if is_unhappy_cell(oi, oj, matrix) and matrix[oi][oj] != (255, 255, 255) and not((oi, oj) in unhappy_cells):
+                unhappy_cells.append((oi, oj))
+            elif not(is_unhappy_cell(oi, oj, matrix)) and (oi, oj) in  unhappy_cells:
+                unhappy_cells.remove((oi, oj))
+    return matrix, unhappy_cells, empty_cells
+
 
 # moving a given number of unlucky cells
 def relocation(matrix, iter_k):
+    plt.imshow(matrix)
+    plt.savefig('img_iteration/matrix_' + str(0) + '.png')
+    unhappy_cells, empty_cells = cell_separation(matrix)
     for k in range(iter_k):
-        unhappy_cells, empty_cells = cell_separation(matrix)
-        matrix = choosing_migrant(matrix, unhappy_cells, empty_cells)
-        if k%1000 == 0:
+        matrix, unhappy_cells, empty_cells = choosing_migrant(matrix, unhappy_cells, empty_cells)
+        if (k + 1) % iteration_frequency == 0:
             plt.imshow(matrix)
-            plt.savefig('img_iteration/matrix_' + str(k) + '.png')
-    return matrix
+            plt.savefig('img_iteration/matrix_' + str(k + 1) + '.png')
+        if len(unhappy_cells) == 0:
+            plt.imshow(matrix)
+            plt.savefig('img_iteration/matrix_' + str(k + 1) + '.png')
+            return print('Все клетки счастливы! \n Остановка на ', k + 1, ' итерации')
 
-
+delete_previous_experiment()
 matrix = matrix_creation(n)
 matrix = matrix.tolist()
 matrix = matrix_conversion(matrix)
-plt.imshow(matrix)
-plt.savefig('img_iteration/matrix_start.png')
 matrix = relocation(matrix, iter_k)
-plt.imshow(matrix)
-plt.savefig('img_iteration/matrix_end.png')
